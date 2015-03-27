00ba855e4e75265d8ebd9fcc3790f66d4049b64d
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-27 12:00:29 +0100

    Création du fichier Exercice1_Main et début d'implémentation.

---
+++ src/exercice_1/Exercice_2_Main.java
@@ -0,0 +1,19 @@
+package exercice_1;
+
+public class Exercice_2_Main {
+
+    
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    
+    public static Graphe Algo_MST(final Graphe g) {
+        Graphe tmp = new Graphe();
+        
+        return g;
+        
+    }
+
+}
