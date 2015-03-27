dd1767ccf781ab5cb8c0eafb6fbb416b67aaee73
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-27 10:47:41 +0100

    Modification dans Edge.java : adaptation de la classe par rapport à la nouvelle classe Vertex.java.

--- src/exercice_1/Edge.java
+++ src/exercice_1/Edge.java
@@ -4,8 +4,8 @@ public class Edge {
     
     //ATTRIBUTS
     
-    private int vertex_1;
-    private int vertex_2;
+    private Vertex vertex_1;
+    private Vertex vertex_2;
     
     
     //CONSTRUCTEUR
@@ -14,7 +14,7 @@ public class Edge {
      * @param vertex_1
      * @param vertex_2
      */
-    public Edge(int vertex_1, int vertex_2) {
+    public Edge(Vertex vertex_1, Vertex vertex_2) {
         super();
         this.vertex_1 = vertex_1;
         this.vertex_2 = vertex_2;
@@ -23,10 +23,10 @@ public class Edge {
     
     //GETTER
     
-    protected int getVertex_1() {
+    protected Vertex getVertex_1() {
         return vertex_1;
     }
-    protected int getVertex_2() {
+    protected Vertex getVertex_2() {
         return vertex_2;
     }
     
