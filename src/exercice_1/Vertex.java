088d1ebb80a5f431ea8dcee5459db353cb17d217
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-27 10:45:54 +0100

    Création de la classe Vertex.java qui represente les sommet d'un graphe.

---
+++ src/exercice_1/Vertex.java
@@ -0,0 +1,28 @@
+package exercice_1;
+
+public class Vertex {
+
+    //ATTRIBUTS
+    
+    private int number;
+
+    
+    //CONSTRUCTOR
+    
+    /**
+     * @param number : vertex number
+     */
+    public Vertex(int number) {
+        super();
+        this.number = number;
+    }
+
+    //METHODES
+
+    public int getNumber() {
+        return number;
+    }
+
+    
+
+}
