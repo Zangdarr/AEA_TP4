e5daa70e342fdeb1cf4b0b0ba0a44f39c61a00be
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-27 10:27:41 +0100

    Création du fichier Graphe.java qui représentera les graphes. Cette classe implémente l'interface GrapheInt.

---
+++ src/exercice_1/Graphe.java
@@ -0,0 +1,45 @@
+package exercice_1;
+
+import java.util.Iterator;
+
+import sun.security.provider.certpath.Vertex;
+
+public class Graphe implements GrapheInt {
+
+    @Override
+    public void addVertex() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addVertexNumber(int i) throws VertexAlreadyExistException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addEdge(Vertex v1, Vertex v2) throws VertexNotFoundException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void addEdge(int i, int j) throws VertexNotFoundException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Vertex getVertex(int i) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Iterator<Edge> getSortedEdgeIterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
