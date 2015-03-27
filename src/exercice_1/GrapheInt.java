0245342ed29febaf146998d0dfd55e57f1ffeb0b
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-27 11:55:28 +0100

    GrapheInt.java : modification de la signature des méthode d'ajout des arêtes. Prise en compte du poids de celles-ci.

--- src/exercice_1/GrapheInt.java
+++ src/exercice_1/GrapheInt.java
@@ -5,9 +5,9 @@ import java.util.Iterator;
 
 public interface GrapheInt {
     public void addVertex();
-    public void addVertexNumber(int i)        throws VertexAlreadyExistException;
-    public void addEdge(Vertex v1, Vertex v2) throws VertexNotFoundException;
-    public void addEdge(int i, int j)         throws VertexNotFoundException;
+    public void addVertexNumber(int i)                   throws VertexAlreadyExistException;
+    public void addEdge(Vertex v1, Vertex v2, int poids) throws VertexNotFoundException;
+    public void addEdge(int i, int j, int p)             throws VertexNotFoundException;
     //...
     public Vertex getVertex(int i);
     //...
