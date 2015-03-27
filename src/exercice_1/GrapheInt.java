c47e97d5d99e50a2fe7ca0e09c960925130adf1d
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-27 10:24:16 +0100

    Création de l'interface que devront respecter les graphes.

---
+++ src/exercice_1/GrapheInt.java
@@ -0,0 +1,18 @@
+package exercice_1;
+
+import java.util.Iterator;
+
+import sun.security.provider.certpath.Vertex;
+
+public interface GrapheInt {
+    public void addVertex();
+    public void addVertexNumber(int i)        throws VertexAlreadyExistException;
+    public void addEdge(Vertex v1, Vertex v2) throws VertexNotFoundException;
+    public void addEdge(int i, int j)         throws VertexNotFoundException;
+    //...
+    public Vertex getVertex(int i);
+    //...
+    public Iterator<Edge> getSortedEdgeIterator();
+    //...
+}
+
