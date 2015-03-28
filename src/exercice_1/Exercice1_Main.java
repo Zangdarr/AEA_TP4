beb43ca532159aabcafc38d856fddfcddf8edb46
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-28 18:10:31 +0100

    Main exo1 : création d'une fonction qui produit un graphe à partir d'un fichier.

--- src/exercice_1/Exercice1_Main.java
+++ src/exercice_1/Exercice1_Main.java
@@ -76,14 +76,47 @@ public class Exercice1_Main {
                     iterator = g.getSortedEdgeIterator();
                 }
             }
-            
-            
-            
+        }
+        
+        return tmp;
+        
+    }
+
+    /**
+     * Produit un graphe à partir d'un fichier
+     * @param filename : nom du fichier
+     * @return
+     * @throws IOException
+     * @throws NumberFormatException : si le fichier ne contient pas que des nombres avec des espace entre eux
+     * @throws VertexAlreadyExistException : si un vertex est placé deux fois en première colonne.
+     * @throws VertexNotFoundException
+     * @throws EdgeAlreadyExistException : si deux arête identique sont écrite dans le fichier.
+     */
+    public static Graphe fileToGraph(String filename) throws IOException, NumberFormatException, VertexAlreadyExistException, VertexNotFoundException, EdgeAlreadyExistException{
+        Graphe tmp = new Graphe();
+        
+        BufferedReader buff = new BufferedReader(new FileReader(filename));
+        String line = "";
+        String[] tab;
+
+        while ((line = buff.readLine()) != null) {
+            tab = line.split(" ");
+            int vertex_1 = Integer.parseInt(tab[0]),
+                vertex_2 = -1;
+            if(!tmp.vertexContains(vertex_1))
+                tmp.addVertexNumber(vertex_1);
+            for (int i = 1; i < tab.length-1; i+=2) {
+                vertex_2 = Integer.parseInt(tab[i]);
+                if(!tmp.vertexContains(vertex_2))
+                    tmp.addVertexNumber(vertex_2);
+                tmp.addEdge(tmp.getVertex(vertex_1), tmp.getVertex(vertex_2), Integer.parseInt(tab[i+1]));
+            }
             
         }
         
-        return g;
+        buff.close();
         
+        return tmp;
     }
 
 }
