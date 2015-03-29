78ee1a796cbc4905282ca4b0fa451e7dbdd2361e
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-29 22:11:51 +0200

    Main Exo2 : mise à jour.

--- src/main/Exercice2_Main.java
+++ src/main/Exercice2_Main.java
@@ -1,7 +1,12 @@
 package main;
 
+import java.io.IOException;
+
+import exceptions.EdgeAlreadyExistException;
+import exceptions.VertexNotFoundException;
 import graphe.Graphe;
 import graphe.RandomGraphGenerator;
+import tools.GraphTools;
 import tools.MSTTools;
 
 public class Exercice2_Main {
@@ -9,12 +14,22 @@ public class Exercice2_Main {
     public static void main(String[] args) {
         RandomGraphGenerator gen = new RandomGraphGenerator();
         System.out.println("Génération du graphe en cours...\n");
-        Graphe g = gen.generateErdosRenyiGraph(100, (float) 1.0);
+        Graphe g = gen.generateErdosRenyiGraph(100, (float) 0.5);
         
         System.out.println("\nApplication de l'algorithme PRIM...\n\n");
-        Graphe result = MSTTools.runPRIM(g);
+        Graphe result = new Graphe();
+        try {
+            result = MSTTools.runPRIM(g);
+            System.out.println("Initialisation, " + g.toStringMinimal() + "\n\nRésultat du PRIM, " + result.toStringMinimal());
         
-        System.out.println("Initialisation, " + g.toStringMinimal() + "\n\nRésultat du PRIM, " + result.toStringMinimal());
+        
+        } catch (VertexNotFoundException | EdgeAlreadyExistException e1) {System.err.println("Erreur lors de l'application de l'algorithme PRIM  : " + e1.getMessage());}
+        
+        
+        try {
+            GraphTools.graphToFile("init_graphe", g);
+            GraphTools.graphToFile("export_exo2", result);
+        } catch (IOException e) {System.err.println("Erreur lors de la création d'un fichier graphe. " + e.getMessage());}
     }
     
     
