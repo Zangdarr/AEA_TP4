3d1ab7c64a3b20a4237d7e6804c5d2a2dcac0f10
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-29 18:33:34 +0200

    Exercice 2 MAIN : génération d'un graphe aléatoire. Application de PRIM dessus.

--- src/main/Exercice2_Main.java
+++ src/main/Exercice2_Main.java
@@ -1,11 +1,19 @@
 package main;
 
 import graphe.Graphe;
+import graphe.RandomGraphGenerator;
 
 public class Exercice2_Main {
 
     public static void main(String[] args) {
+        RandomGraphGenerator gen = new RandomGraphGenerator();
+        System.out.println("Génération du graphe en cours...\n");
+        Graphe g = gen.generateErdosRenyiGraph(100, (float) 1.0);
         
+        System.out.println("\nApplication de l'algorithme PRIM...\n\n");
+        Graphe result = Exercice1_Main.Algo_PRIM(g);
+        
+        System.out.println("Initialisation, " + g.toStringMinimal() + "\n\nRésultat du PRIM, " + result.toStringMinimal());
     }
     
     
