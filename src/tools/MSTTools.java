e56a1ceb812366aaa1017080097b7de96fadec22
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-29 22:05:32 +0200

    MSTTools : changement du nom de l'algorithme, modification des commentaires, changement du nom d'une variable.
    - le précédent nom été PRIM cependant cette algo ne respecte pas exactement la spec donc je l'ai changer
    - le commentaire décrivant la fonction à donc été modifié en conséquent
    - le nom de variable tmp ne me convenait pas pour le graphe à retourner

--- src/tools/MSTTools.java
+++ src/tools/MSTTools.java
@@ -14,53 +14,53 @@ import java.util.Iterator;
 public class MSTTools {
 
     /**
-     * Applique l'algorithme de calcul d'arbre recouvrant de poids minimum PRIM sur un graphe
+     * Applique un algorithme de calcul d'arbre recouvrant de poids minimum sur un graphe. Cette algorithme est inspiré de PRIM et KRUSKAL
      * @param g : graphe sur lequel sera appliqué l'algorithme
      * @return
      */
-    public static Graphe runPRIM(final Graphe g) {
-        Graphe tmp = new Graphe();
+    public static Graphe runVERKYNDT(final Graphe g) {
+        Graphe result = new Graphe();
         /****** INIT *****/
         Iterator<Edge> it = g.getSortedEdgeIterator();
-        
+
         if(!it.hasNext()){
             System.out.println("ERROR : Liste vide");
         }
         //on prend l'arête de poids le plus faible    
         Edge first_e = (Edge)it.next();
         try {
-            tmp.addVertexNumber(first_e.getVertex_1().getId());
-            tmp.addVertexNumber(first_e.getVertex_2().getId());
-            tmp.addEdge(first_e.getVertex_1(), first_e.getVertex_2(), first_e.getPoids());
+            result.addVertexNumber(first_e.getVertex_1().getId());
+            result.addVertexNumber(first_e.getVertex_2().getId());
+            result.addEdge(first_e.getVertex_1(), first_e.getVertex_2(), first_e.getPoids());
         } catch (VertexAlreadyExistException | VertexNotFoundException | EdgeAlreadyExistException e2) {
             // TODO Auto-generated catch block
             System.out.println(e2.getMessage());
         }
-        
-        
+
+
         /******* MAIN ******/
-        //on prend l'arête de poids la plus faible qui appartient à l'ensemble déjà présent dans tmp
-                                                                                                                                                                                                                                                                                
-        for (Iterator<Edge> iterator = it ; iterator.hasNext() && g.getVertexQuantity() > tmp.getVertexQuantity();) {
+        //on prend l'arête de poids la plus faible qui possède un sommet appartenant à l'ensemble déjà présent dans result
+
+        for (Iterator<Edge> iterator = it ; iterator.hasNext() && g.getVertexQuantity() > result.getVertexQuantity();) {
             Edge e = (Edge) iterator.next();
-            
-            boolean v1_exist_inside = tmp.vertexContains(e.getVertex_1().getId()),
-                    v2_exist_inside = tmp.vertexContains(e.getVertex_2().getId());
+
+            boolean v1_exist_inside = result.vertexContains(e.getVertex_1().getId()),
+                    v2_exist_inside = result.vertexContains(e.getVertex_2().getId());
 
             //si on a un sommet présent dans l'ensemble et l'autre non
             if( (v1_exist_inside && !v2_exist_inside) || (! v1_exist_inside && v2_exist_inside) ){
                 //si on a v1 alors on rajoute v2
                 if(v1_exist_inside){
                     try {
-                        tmp.addVertexNumber(e.getVertex_2().getId());
-                        tmp.addEdge(e.getVertex_1(), e.getVertex_2(), e.getPoids());
+                        result.addVertexNumber(e.getVertex_2().getId());
+                        result.addEdge(e.getVertex_1(), e.getVertex_2(), e.getPoids());
                     } catch (VertexAlreadyExistException | VertexNotFoundException | EdgeAlreadyExistException e1) {System.out.println(e1.getMessage());}
                     iterator = g.getSortedEdgeIterator();
                 }
                 else{
                     try {
-                        tmp.addVertexNumber(e.getVertex_1().getId());
-                        tmp.addEdge(e.getVertex_1(), e.getVertex_2(), e.getPoids());
+                        result.addVertexNumber(e.getVertex_1().getId());
+                        result.addEdge(e.getVertex_1(), e.getVertex_2(), e.getPoids());
                     } catch (VertexAlreadyExistException | VertexNotFoundException | EdgeAlreadyExistException e1) {System.out.println(e1.getMessage());}
                     iterator = g.getSortedEdgeIterator();
                 }
