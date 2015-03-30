9addfea320bfde90cd2d9fd9527e85903375c676
Zangdarr <alexandre.verkyndt@gmail.com>
2015-03-30 16:51:33 +0200

    Exo2_main : méthode exemplePrim ne génère plus de graphe. Un main existe pour générer les graphes.

--- src/main/Exercice2_Main.java
+++ src/main/Exercice2_Main.java
@@ -16,13 +16,37 @@ public class Exercice2_Main {
     public static void main(String[] args) {
         exemplePRIM();
     }
+    
+    
+    
+    
+    
+    
+    public static void exemplePRIM(){
+        System.out.println("PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  ");
+        Graphe g = new Graphe();
+        try {
+            g = GraphTools.fileToGraph("1000vertex_100pctedges.gph");
+        } catch (NumberFormatException | IOException
+                | VertexAlreadyExistException | VertexNotFoundException
+                | EdgeAlreadyExistException e2) {
+            System.out.println(e2.getMessage());
+        }
         Graphe result = new Graphe();
         try {
             result = MSTTools.runPRIM(g);
-            System.out.println("Initialisation, " + g.toStringMinimal() + "\n\nRésultat du PRIM, " + result.toStringMinimal());
-        
-        
+            System.out.println("Initialisation, " + g.toStringMinimal() + "\n\nResult of PRIM, " + result.toStringMinimal());
         } catch (VertexNotFoundException | EdgeAlreadyExistException e1) {System.err.println("Erreur lors de l'application de l'algorithme PRIM  : " + e1.getMessage());}
+        
+        
+        try {
+            GraphTools.graphToFile("init_PRIM.gph", g);
+            GraphTools.graphToFile("result_PRIM.gph", result);
+        } catch (IOException e) {System.err.println("Erreur lors de la création d'un fichier graphe. " + e.getMessage());}
+    
+        System.out.println("PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  ");
+
+    }
         
         
         try {
