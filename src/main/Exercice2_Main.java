b1529ec3a5756df0184280e94436f05deadde8e4
Zangdarr <alexandre.verkyndt@gmail.com>
2015-04-08 21:10:51 +0200

    main : application de Kruskal sur le graphe complet de 2000 sommets -> moins d'une seconde

--- src/main/Exercice2_Main.java
+++ src/main/Exercice2_Main.java
@@ -14,7 +14,7 @@ import tools.MSTTools;
 public class Exercice2_Main {
 
     public static void main(String[] args) {
-        callKRUSKAL("1000x1000.gph");
+        callKRUSKAL_OPTIMAL("2000x2000.gph");
     }
     
     
@@ -75,6 +75,32 @@ public class Exercice2_Main {
 
     }
     
+    public static void callKRUSKAL_OPTIMAL(String graphFileName){
+        System.out.println("KRUSKAL OPTI  KRUSKAL OPTI KRUSKAL OPTI KRUSKAL OPTI KRUSKAL OPTI KRUSKAL OPTI KRUSKAL  ");
+        Graphe g = null;
+        try {
+            g = GraphTools.fileToGraph(graphFileName);
+        } catch (NumberFormatException | IOException
+                | VertexAlreadyExistException | VertexNotFoundException
+                | EdgeAlreadyExistException e2) {
+            System.out.println(e2.getMessage());
+        }
+        
+        Graphe result = new Graphe();
+        try {
+            result = MSTTools.runKRUSKAL_OPTIMAL(g);
+            System.out.println("Initialisation, " + g.toStringMinimal() + "\n\nResul of KRUSKAL OPTI, " + result.toStringMinimal());
+        } catch (VertexNotFoundException | EdgeAlreadyExistException | GrapheException e1) {System.err.println("Erreur lors de l'application de l'algorithme KRUSKAL OPTI : " + e1.getMessage());}
+        
+        
+        try {
+            GraphTools.graphToFile("init_KRUSKAL.gph", g);
+            GraphTools.graphToFile("result_KRUSKAL.gph", result);
+        } catch (IOException e) {System.err.println("Erreur lors de la création d'un fichier graphe. " + e.getMessage());}
+    
+        System.out.println("KRUSKAL OPTI KRUSKAL OPTI KRUSKAL OPTI KRUSKAL OPTI KRUSKAL OPTI KRUSKAL OPTI KRUSKAL  ");
+
+    }
     
     
     
