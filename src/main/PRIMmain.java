dada11118b941fd5eba7fcc3272550015c33deef
Zangdarr <alexandre.verkyndt@gmail.com>
2015-04-18 14:15:09 +0200

    Ajout du main pour PRIM.

---
+++ src/main/PRIMmain.java
@@ -0,0 +1,39 @@
+package main;
+
+import exceptions.EdgeAlreadyExistException;
+import exceptions.VertexAlreadyExistException;
+import exceptions.VertexNotFoundException;
+import graphe.Graphe;
+
+import java.io.IOException;
+
+import tools.GraphTools;
+import tools.MSTTools;
+
+public class PRIMmain {
+
+    public static void main(String[] args) {
+        callPRIM("500x50.gph");
+    }
+    
+    
+    public static void callPRIM(String graphFileName){
+        System.out.println("PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  ");
+        Graphe g = new Graphe();
+        try {
+            g = GraphTools.fileToGraph(graphFileName);
+        } catch (NumberFormatException | IOException
+                | VertexAlreadyExistException | VertexNotFoundException
+                | EdgeAlreadyExistException e2) {
+            System.out.println(e2.getMessage());
+        }
+        Graphe result = new Graphe();
+        try {
+            result = MSTTools.runPRIM(g);
+            System.out.println("Initialisation, " + g.toStringMinimal() + "\n\nResult of PRIM, " + result.toStringMinimal());
+        } catch (VertexNotFoundException | EdgeAlreadyExistException e1) {System.err.println("Erreur lors de l'application de l'algorithme PRIM  : " + e1.getMessage());}
+    
+        System.out.println("PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  PRIM  ");
+
+    }
+}
